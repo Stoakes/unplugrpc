84e75da7267437096a4f00cdb765e881036b1943
stoakes <Stoakes@users.noreply.github.com>
2018-08-14 19:57:52 +0200

    feat(gateway): Add packages list & show API

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -5,8 +5,9 @@ import expressValidator from "express-validator";
 import path from "path";
 
 import * as dashboardController from "./controllers/dashboard";
-import * as protoController from "./controllers/proto";
 import * as hostsController from "./controllers/hosts";
+import * as packagesController from "./controllers/packages";
+import * as protoController from "./controllers/proto";
 
 const newApp = (corsEnabled: boolean): express.Express => {
     const app = express();
@@ -23,12 +24,17 @@ const newApp = (corsEnabled: boolean): express.Express => {
     // Routes
     app.get("/dashboard", dashboardController.index);
     app.post("/new", protoController.add);
+    // These are the core methods, they have no side effects on the gateway state.
     app.post("/api/:package.:service/:method/", protoController.api);
     app.get("/api/:package.:service/:method/", protoController.describe);
+    // CRUD on app objects
     app.get("/hosts", hostsController.list);
     app.post("/hosts", hostsController.add);
     app.put("/hosts/:host\::port", hostsController.update);
     app.delete("/hosts/:host\::port", hostsController.remove);
+    app.get("/packages", packagesController.list);
+    app.get("/packages/:name", packagesController.show);
+    // Static files
     app.use("/", express.static(path.join(__dirname, "/../build-ui")));
 
     return app;
---
+++ gateway/src/controllers/packages.ts
@@ -0,0 +1,50 @@
+/**
+ * File for API points about gRPC package
+ */
+import { Request, Response } from "express";
+
+import * as dbService from "../services/dbService";
+import { PROTO_FOLDER } from "../config/config";
+
+/**
+ * List every package stored in database
+ * @param req 
+ * @param res 
+ */
+export const list = (req: Request, res: Response) => {
+
+    const packages = dbService.get("packages").value();
+    if (packages === undefined) {
+        res.json({});
+        return;
+    }
+    res.json(packages.map((item: any) => {
+        return {name: item.name};
+    }));
+};
+
+/**
+ * Show the details of a package.
+ * This methods does not return the full package object, but only:
+ *   - its attributes (PROTO_Folder truncated from filePath)
+ *   - its services and their attributes
+ * @param req 
+ * @param res 
+ */
+export const show = (req: Request, res: Response) => {
+
+    const pack = dbService.get("packages")
+    .find({name: req.params.name})
+    .value();
+    if (pack === undefined) {
+        res.json({level: `error`, message: `Package not found`});
+        return;
+    }
+    res.json({
+        ...pack,
+        filePath: pack.filePath.replace(PROTO_FOLDER, ""),
+        schema: {
+            services: pack.schema.services,
+        }
+    });
+};
